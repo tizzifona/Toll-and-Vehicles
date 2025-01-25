68dfd7c2386f9eece492fdd0598d78c2313655fb
tizzifona <tizzifona@gmail.com>
2025-01-25 09:49:11 +0100

    Added code for services

---
+++ src/main/java/projects/f5/vehicles_java/service/TollStation.java
@@ -0,0 +1,54 @@
+package projects.f5.vehicles_java.service;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import projects.f5.vehicles_java.models.Vehicle;
+
+class TollStation {
+    private String name;
+    private String city;
+    private int totalCollected;
+    private List<Vehicle> vehicles;
+
+    public TollStation(String name, String city) {
+        this.name = name;
+        this.city = city;
+        this.totalCollected = 0;
+        this.vehicles = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getTotalCollected() {
+        return totalCollected;
+    }
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void registerVehicle(Vehicle vehicle) {
+        int toll = vehicle.calculateToll();
+        totalCollected += toll;
+        vehicles.add(vehicle);
+    }
+
+    public int getTotal() {
+        return totalCollected;
+    }
+
+    public void listVehicles() {
+        System.out.println("Vehicles registered at " + name + ":");
+        for (Vehicle v : vehicles) {
+            System.out.println("- " + v.getRegistrationPlate() + " | Toll: $" + v.calculateToll());
+        }
+        System.out.println("Total collected: $" + totalCollected);
+    }
+}
