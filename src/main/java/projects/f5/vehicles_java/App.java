208b0f5b69ed311a71a66033fb6119e634b5fe0f
tizzifona <tizzifona@gmail.com>
2025-01-25 08:59:16 +0100

    Added main structure

---
+++ src/main/java/projects/f5/vehicles_java/App.java
@@ -0,0 +1,17 @@
+package projects.f5.vehicles_java;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/projects/f5/vehicles_java/AppTest.java
@@ -0,0 +1,18 @@
+package projects.f5.vehicles_java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
